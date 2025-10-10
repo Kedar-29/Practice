9dce4dc5145cf07b7f274801a7ed644623956dc5
Kedar Mhetre <kedarsm2020@gmail.com>
2025-10-10 22:38:04 +0530

    qr updates

--- qr/app/layout.tsx
+++ qr/app/layout.tsx
@@ -7,15 +7,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        style={{
-          margin: 0,
-          padding: 0,
-          background: "#fff",
-        }}
-      >
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
--- qr/app/page.tsx
+++ qr/app/page.tsx
@@ -12,15 +12,7 @@ export default function Page() {
   }));
 
   return (
-    <main
-      style={{
-        margin: 0,
-        padding: 0,
-        background: "#fff",
-        width: "100%",
-        height: "100%",
-      }}
-    >
+    <main>
       <QRLabelSheet data={data} />
     </main>
   );
